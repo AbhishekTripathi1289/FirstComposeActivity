22beb7fd6d91081bdac2ffa549edb22b8ad7882b
Abhishek Tripathi <tripathiabhishek1289@gmail.com>
2024-01-03 15:08:35 +0530

    Commit Code

--- app/src/main/java/com/example/firstcomposeactivity/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeactivity/MainActivity.kt
@@ -1,62 +1,42 @@
 package com.example.firstcomposeactivity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.firstcomposeactivity.compose.ComposeWithXml
+import com.example.firstcomposeactivity.compose.ConstraintLayoutExample
+import com.example.firstcomposeactivity.compose.LazyVerticalGridExample
+import com.example.firstcomposeactivity.compose.PracticeCompose
 import com.example.firstcomposeactivity.ui.theme.FirstComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeActivityTheme {
-
-                    Column(modifier = Modifier.width(10.dp).background(Color.Green),
-                        horizontalAlignment = Alignment.End,
-                        verticalArrangement = Arrangement.Center) {
-                        Greeting("Android")
-                        Greeting("dfasdfsa")
-                    }
-
-            }
+            LazyVerticalGridExample()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstComposeActivityTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Greeting("Android")
+    @Preview(showBackground = true, showSystemUi = true,)
+    @Composable
+    fun GreetingPreview() {
+        FirstComposeActivityTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+             //   ButtonInComposable()
+            }
         }
     }
 }
